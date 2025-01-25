c1bca59127a980a498acb28fe8fbe971721e8fec
Mihir Khare <mihir00khare@gmail.com>
2025-01-24 19:52:37 -0500

    updated optimization script to use a temp directory

--- optimize.sh
+++ optimize.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
+# Extract arguments
 WORKLOAD=$1
 OUTPUT_DIR=$2
+
+# Make a temporary directory for intermediate results
+TEMP_DIR=$(mktemp -d -p .)
 
 echo "Invoking optimize.sh."
 echo -e "\tWorkload: ${WORKLOAD}"
 echo -e "\tOutput Dir: ${OUTPUT_DIR}"
 
 mkdir -p "${OUTPUT_DIR}"
-mkdir -p input/
 
 # Extract the workload.
-tar xzf "${WORKLOAD}" --directory input/
+tar xzf "${WORKLOAD}" --directory $TEMP_DIR/
 
 # Feel free to add more steps here.
 
@@ -22,5 +24,8 @@ cd calcite_app/
 ./gradlew build
 ./gradlew shadowJar
 ./gradlew --stop
-java -Xmx4096m -jar build/libs/calcite_app-1.0-SNAPSHOT-all.jar "../input/queries" "../${OUTPUT_DIR}"
+java -Xmx4096m -jar build/libs/calcite_app-1.0-SNAPSHOT-all.jar "../${TEMP_DIR}/queries" "../${OUTPUT_DIR}"
 cd -
+
+# Clean up temporary data
+rm -rf $TEMP_DIR
